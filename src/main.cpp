bd5d0e4b67eb9c1f20fa6629724c4c548f725285
centaurea9 <centaurea1016@gmail.com>
2026-07-02 13:52:05 +0800

    固定黄框

--- src/main.cpp
+++ src/main.cpp
@@ -22,38 +22,40 @@ void setup() {
 }
 
 void loop() {
-    tft.fillScreen(TFT_WHITE);
+   
 
     // 显示图片 164x76, 1-bit 位图
-    tft.drawBitmap(60, 0, epd_bitmap_, 164, 76, TFT_WHITE,TFT_BLACK );
+    // tft.fillScreen(TFT_WHITE);
+   // tft.drawBitmap(60, 0, epd_bitmap_, 164, 76, TFT_WHITE,TFT_BLACK );
+   // delay(2000);
+    tft.fillScreen(TFT_BLACK);
 
-    // //长条屏幕x=90px左侧
-    //
-    // //显示星期
-    // u8g2.setFont(u8g2_font_wqy16_t_gb2312);
-    // screen_text_color(TFT_CYAN, TFT_BLACK);
-    // screen_draw_text(22, 15, "星期天");
-    //
-    // //显示时间
-    // u8g2.setFont(u8g2_font_freedoomr25_tn);
-    // screen_text_color(TFT_CYAN, TFT_BLACK);
-    // screen_draw_text(-3, 40, "10:16");
-    //
-    // // ---------- 竖线 ----------
-    // tft.drawFastVLine(89, 0, 76, TFT_CYAN);
-    // screen_text_color(TFT_CYAN, TFT_BLACK);
-    //
+    
+    //长条屏幕x=90px左侧
+     //显示星期
+     u8g2.setFont(u8g2_font_wqy16_t_gb2312);
+    screen_text_color(TFT_CYAN, TFT_BLACK);
+     screen_draw_text(22, 15, "星期天");
+   
+     //显示时间
+     u8g2.setFont(u8g2_font_freedoomr25_tn);
+     screen_text_color(TFT_CYAN, TFT_BLACK);
+     screen_draw_text(-3, 40, "10:16");
+   
+     // ---------- 竖线 ----------
+     tft.drawFastVLine(89, 0, 76, TFT_CYAN);
+     screen_text_color(TFT_CYAN, TFT_BLACK);
+    
     // //x=90px右侧
-    // u8g2.setFont(u8g2_font_wqy12_t_gb2312);
-    // screen_draw_text(200,10, "功能1");
-    // screen_draw_text(190,31, "功能2");
-    // screen_draw_text(200,52, "功能3");
-    //
-    // // ---------- 斜线 ----------
-    // tft.drawLine(100, 60, 180, 70, TFT_RED);
-    //
-    // // ---------- 矩形 ----------
-    // tft.drawRect(100, 5, 80, 30, TFT_YELLOW);
+     u8g2.setFont(u8g2_font_wqy12_t_gb2312);
+     screen_draw_text(185,7, "天气预报");
+     screen_draw_text(175,31, "干员寻访");
+     screen_draw_text(185,56, "系统设置");
+    
+     // ---------- 斜线 ----------
+     //tft.drawLine(100, 60, 180, 70, TFT_RED);
+    // ---------- 矩形 ----------
+     tft.drawRect(94, 24, 186, 22, TFT_YELLOW);
 
     delay(10000);
 }
